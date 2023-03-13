2a3bc82986a6cab1433d09f820058196ff63084b
Ethan Daniel <e.daniel.small@gmail.com>
2023-03-12 21:41:09 -0700

    js questions

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+
+// questions: 
+// what was javascript built off of? java, typecript, python, or ecmascript? (ecmascript)
+// 
+// "undefined" is a reserved word? true, false (false)
+// 
+// what does NaN stand for? not a number, near area network, none as null, next alert now (not a number)
+// 
+// how long did it take to develop javascript? 10 days, 10 weeks, 10 months, 10 years (10 days)
+// 
+// can numbers be put into a string? yes, no (yes)
+//
+// when was javacript invented and released? 1995, 1999, 2010, 2015 (1995)
+// 
+// why was javascript made? to improve upon ecmascript, to handle more traffic to disneyland's website, for a specific company's browser, to help the robot sofia learn langage (for a specific company's browser)
+// 
+// what was javascript's original name going to be? mocha, spock, eich, energi (mocha)
+// 
+// brendan eich created javascript. *fun fact here*
+// 
+// when did brendan eich learn programming middle-highschool, college, late 20's, early 40's (college)
+// 
+// who created javascript? brendan eich, tucker beauchamp, guido van rossum, brent spiner (brendan eich)
+// 
+// what do arrays go inside of? brackets, double quotes, single quotes, parenthases
+// 
+// w
+// 
+// w
+// 
+// w
