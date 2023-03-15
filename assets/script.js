1ebff6d1f4ca0f9e90ff93557ada275d661d4b34
Ethan Daniel <e.daniel.small@gmail.com>
2023-03-15 08:52:35 -0700

    added a start buttton

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,13 @@
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+    isWin = false;
+    timerCount = 10;
+    // Prevents start button from being clicked when round is in progress
+    startButton.disabled = true;
+    // render questions ??
+    startTimer()
+  }
 
 // Questions will be asked (should be 15 total??)
 const Questions = [{
