d75e369ab131e45d69ca688748d3811b3501d6a3
Ethan Daniel <e.daniel.small@gmail.com>
2023-03-15 18:50:08 -0700

    deleted messy, unusable files

--- assets/script2.js
+++ assets/script2.js
@@ -76,3 +76,135 @@ buildQuiz();
 
 // on submit, show results
 submitButton.addEventListener('click', showResults);
+
+// const Questions = [{
+//   id: 0,
+//   q: "what was javascript built off of?",
+//   a: [{ text: "typescript", isCorrect: false },
+//       { text: "python", isCorrect: false },
+//       { text: "ecmascript", isCorrect: true },
+//       { text: "java", isCorrect: false }
+//   ]
+
+// },
+// {
+//   id: 1,
+//   q: "what do arrays go inside of?",
+//   a: [{ text: "double quotes", isCorrect: false,},
+//       { text: "single quotes", isCorrect: false },
+//       { text: "parentheses", isCorrect: false },
+//       { text: "brackets", isCorrect: true }
+//   ]
+
+// },
+// {
+//   id: 2,
+//   q: "what does NaN stand for?",
+//   a: [{ text: "near area network", isCorrect: false },
+//       { text: "not a number", isCorrect: true },
+//       { text: "none as null", isCorrect: false },
+//       { text: "new alert next", isCorrect: false }
+//   ]
+
+// },
+// {
+//   id: 3,
+//   q: "who made javascript?",
+//   a: [{ text: "tucker beauchamp", isCorrect: false },
+//       { text: "brendan eich", isCorrect: true },
+//       { text: "guido van rossum", isCorrect: false },
+//       { text: "brent spiner", isCorrect: false }
+//   ]
+
+// },
+// {
+//   id: 4,
+//   q: "when did brendan eich learn programming?",
+//   a: [{ text: "middle-high school", isCorrect: false },
+//       { text: "college", isCorrect: true },
+//       { text: "late 20's", isCorrect: false },
+//       { text: "early 40's", isCorrect: false }
+//   ]
+
+// },
+// {
+//   id: 5,
+//   q: "what was javascript's original name going to be?",
+//   a: [{ text: "spock", isCorrect: false },
+//       { text: "energi", isCorrect: false },
+//       { text: "mocha", isCorrect: true },
+//       { text: "eich", isCorrect: false }
+//   ]
+
+// },
+// {
+//   id: 6,
+//   q: "how long did it take to develop javascript?",
+//   a: [{ text: "10 days", isCorrect: true },
+//       { text: "10 weeks", isCorrect: false },
+//       { text: "10 months", isCorrect: true },
+//       { text: "10 years", isCorrect: false }
+//   ]
+
+// },
+// {
+//   id: 7,
+//   q: "when was javascript released?",
+//   a: [{ text: "1975", isCorrect: false },
+//       { text: "1995", isCorrect: true },
+//       { text: "2010", isCorrect: false },
+//       { text: "2015", isCorrect: false }
+//   ]
+
+// },
+// {
+//   id: 8,
+//   q: "why was javascript made?",
+//   a: [{ text: "to improve upon ecmascript", isCorrect: false },
+//       { text: "to handle more traffic to disneyland's website", isCorrect: false },
+//       { text: "to help the robot sofia learn language", isCorrect: false },
+//       { text: "for a specific company's browser", isCorrect: true }
+//   ]
+
+// },
+// {
+//   id: 9,
+//   q: "What is the capital of Gujarat",
+//   a: [{ text: "surat", isCorrect: false },
+//       { text: "vadodara", isCorrect: false },
+//       { text: "gandhinagar", isCorrect: true },
+//       { text: "rajkot", isCorrect: false }
+//   ]
+
+// },
+// {
+//   id: 10,
+//   q: "What is the capital of Gujarat",
+//   a: [{ text: "surat", isCorrect: false },
+//       { text: "vadodara", isCorrect: false },
+//       { text: "gandhinagar", isCorrect: true },
+//       { text: "rajkot", isCorrect: false }
+//   ]
+
+// },
+// {
+//   id: 11,
+//   q: "What is the capital of Gujarat",
+//   a: [{ text: "surat", isCorrect: false },
+//       { text: "vadodara", isCorrect: false },
+//       { text: "gandhinagar", isCorrect: true },
+//       { text: "rajkot", isCorrect: false }
+//   ]
+
+// },
+// {
+//   id: 12,
+//   q: "last question",
+//   a: [{ text: "surat", isCorrect: false },
+//       { text: "vadodara", isCorrect: false },
+//       { text: "gandhinagar", isCorrect: true },
+//       { text: "rajkot", isCorrect: false }
+//   ]
+
+// }
+// ];
